ecc545effae93f19373481d16abfdd4a966c1e2b
Anuraj <anuraj.p@msn.com>
2021-11-27 13:32:24 +0530

    Updated with GraphQL Mutation

--- Program.cs
+++ Program.cs
@@ -3,7 +3,12 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContextFactory<BookmarkDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("BookmarkDbConnection")));
-builder.Services.AddGraphQLServer().AddQueryType<Query>().AddProjections().AddFiltering().AddSorting();
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddProjections()
+    .AddFiltering()
+    .AddSorting()
+    .AddMutationType<Mutation>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
@@ -95,3 +100,76 @@ public class Query
     public IQueryable<Link> Links([ScopedService] BookmarkDbContext bookmarkDbContext)
             => bookmarkDbContext.Links;
 }
+
+public class Mutation
+{
+    [UseDbContext(typeof(BookmarkDbContext))]
+    public async Task<LinkOutput> AddLink(LinkInput linkInput,
+        [ScopedService] BookmarkDbContext bookmarkDbContext)
+    {
+        if (string.IsNullOrEmpty(linkInput.Url))
+        {
+            throw new ArgumentNullException(nameof(linkInput.Url));
+        }
+
+        var link = new Link
+        {
+            Url = linkInput.Url,
+            Title = linkInput.Title,
+            Description = linkInput.Description,
+            ImageUrl = linkInput.ImageUrl,
+            CreatedOn = DateTime.UtcNow
+        };
+        bookmarkDbContext.Links.Add(link);
+        await bookmarkDbContext.SaveChangesAsync();
+        return new LinkOutput(true, "Link created successfully", link.Id, link.Url,
+            link.Title, link.Description, link.ImageUrl, link.CreatedOn);
+    }
+
+    [UseDbContext(typeof(BookmarkDbContext))]
+    public async Task<LinkOutput> UpdateLink(int id, LinkInput linkInput, [ScopedService] BookmarkDbContext bookmarkDbContext)
+    {
+        var link = bookmarkDbContext.Links.Find(id);
+        if (link != null)
+        {
+            if (link.Title != linkInput.Title)
+            {
+                link.Title = linkInput.Title;
+            }
+            if (link.Description != linkInput.Description)
+            {
+                link.Description = linkInput.Description;
+            }
+            if (link.ImageUrl != linkInput.ImageUrl)
+            {
+                link.ImageUrl = linkInput.ImageUrl;
+            }
+            if (link.Url != linkInput.Url)
+            {
+                link.Url = linkInput.Url;
+            }
+
+            await bookmarkDbContext.SaveChangesAsync();
+            return new LinkOutput(true, "Link updated successfully", link.Id, link.Url, link.Title, link.Description, link.ImageUrl, link.CreatedOn);
+        }
+
+        return new LinkOutput(false, "Unable to update the Link.", null, null, null, null, null, null);
+    }
+
+    [UseDbContext(typeof(BookmarkDbContext))]
+    public async Task<LinkOutput> DeleteLink(int id, [ScopedService] BookmarkDbContext bookmarkDbContext)
+    {
+        var link = bookmarkDbContext.Links.Find(id);
+        if (link != null)
+        {
+            bookmarkDbContext.Links.Remove(link);
+            await bookmarkDbContext.SaveChangesAsync();
+            return new LinkOutput(true, "Link deleted successfully.", null, null, null, null, null, null);
+        }
+
+        return new LinkOutput(false, "Link not found.", null, null, null, null, null, null);
+    }
+}
+
+public record LinkInput(string Url = "", string Title = "", string Description = "", string ImageUrl = "");
+public record LinkOutput(bool IsSuccess, string Message, int? Id, string? Url, string? Title, string? Description, string? ImageUrl, DateTime? CreatedOn);
